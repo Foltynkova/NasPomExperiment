79a42e73d50bec0abb166a7f1f48c14483160da0
Helena Foltynkova <helena.foltynkova@gmail.com>
2020-11-12 20:23:28 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/SpousteciTrida.java
@@ -0,0 +1,19 @@
+package cz.czechitas;
+
+import org.openqa.selenium.*;
+
+
+public class SpousteciTrida {
+
+    
+
+    public static void main(String[] args) {
+        System.out.println("Zdravim, pozemstani! Zavedte me ke svemu vudci");
+        WebDriver browser = new FirefoxDriver();
+
+
+    }
+
+    
+
+}
